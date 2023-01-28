f6df8e8711b7432903e114c676d07ef4a39e898f
lxnhard <106260289+lxnhard@users.noreply.github.com>
2023-01-28 15:57:48 +0100

    integrate registration, login components in root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie_app-angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // function that will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '480px'
+    });
+  }
+
+  openLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '480px'
+    });
+  }
 }
