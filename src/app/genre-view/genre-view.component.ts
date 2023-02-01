a450b6b1ed471974e8fc3fc9f32b2be97462dd48
lxnhard <106260289+lxnhard@users.noreply.github.com>
2023-02-01 11:15:19 +0100

    implement basic genre view dialog

---
+++ src/app/genre-view/genre-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+
+
+@Component({
+  selector: 'app-genre-view',
+  templateUrl: './genre-view.component.html',
+  styleUrls: ['./genre-view.component.scss']
+})
+export class GenreViewComponent {
+  genreName: string = "";
+  genreDescription: string = "";
+
+  constructor(
+    private dialogRef: MatDialogRef<GenreViewComponent>, @Inject(MAT_DIALOG_DATA) data: any
+  ) {
+    this.genreName = data.genreName;
+    this.genreDescription = data.genreDescription;
+  }
+}
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiServices } from '../fetch-api-data.service'
+import { MatDialog } from '@angular/material/dialog';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiServices) { }
+  constructor(
+    public fetchApiData: FetchApiServices,
+    public dialog: MatDialog) { }
 
   // lifecycle hook: called after creating component
   ngOnInit(): void {
@@ -21,5 +25,16 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  openGenreDialog(genreName: string, genreDescription: string): void {
+    this.dialog.open(GenreViewComponent, {
+      width: '500px',
+      data: {
+        genreName: genreName,
+        genreDescription: genreDescription
+      }
+    });
+
   }
 }
